478479f2dcd5fd0f0e84b2166af11da4162608e5
phuong3123560067 <lengocphuong6205@gmail.com>
2024-11-02 10:25:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,9 @@
-let slideIndex = 0;
-let slideInterval = setInterval(showSlides, 3000); 
-
-function showSlides() {
-    let slides = document.querySelectorAll(".slide");
-    slides.forEach(slide => slide.style.display = "none");
-
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1; }
-    slides[slideIndex - 1].style.display = "block";
+function showSignUp() {
+    document.getElementById('form-login').classList.add('hidden');
+    document.getElementById('form-signup').classList.remove('hidden');
 }
 
-function nextSlide() {
-    slideIndex++;
-    showSlideManually();
-    resetInterval();
+function showLogin() {
+    document.getElementById('form-signup').classList.add('hidden');
+    document.getElementById('form-login').classList.remove('hidden');
 }
-
-function prevSlide() {
-    slideIndex--;
-    showSlideManually();
-    resetInterval();
-}
-
-function showSlideManually() {
-    let slides = document.querySelectorAll(".slide");
-    slides.forEach(slide => slide.style.display = "none");
-
-    if (slideIndex >= slides.length) { slideIndex = 0; }
-    if (slideIndex < 0) { slideIndex = slides.length - 1; }
-    slides[slideIndex].style.display = "block";
-}
-
-function resetInterval() {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(showSlides, 3000); 
-}
-
-function product()
-{
-    window.location.href = 'product.html';
-}
- function shortcake()
- {
-    window.location.href = 'shortcake.html';
- }
