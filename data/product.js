4d662440faf2cc75a6bd05b88fec2c542a2df6e1
vinh3123410433 <nguyenvinhthai11a8@gmail.com>
2024-11-06 07:49:20 +0700

    Update product.js

--- data/product.js
+++ data/product.js
@@ -3,7 +3,7 @@ var list_products = [{
     name: "Corn Mousse Cake",
     price: "320000",
     image: "image/product/corn-mousse.png",
-    type:"Cake",
+    type:"Whole Cake",
     description: "Bánh kem mousse bắp, kem tươi và hạnh nhân",
     taste: "Ngọt",
     size: "16cm",
@@ -11,25 +11,41 @@ var list_products = [{
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
+},
+{
     id: 2,
     name: "Fresh Mango Cream Cake",
-    price: "350000" ,
+    price: "350000",
     image: "image/product/FRESH-MANGO.png",
-    type:"Cake",
+    type:"Whole Cake",
     description: "Bánh kem bông lan vani kem tươi và mứt trái cây",
     taste: "Ngọt",
     size: "16cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
-    }  
-},{
+    }
+    },
+{
     id: 3,
+    name: "Black Forest Cake",
+    price: "340000",
+    image: "image/product/BLACK-FOREST.png     ",
+    type:"Whole Cake",
+    description: "Bánh kem sô cô la cùng kem tươi và mứt cherry",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 4,
     name: "Fresh Strawberry Yogurt Cream Cake",
     price: "400000",
-    image: "image/product/YOGURT-CREAM.png",
-    type:"Cake",
+    image: "image/product/YOGURT.png",
+    type:"Whole Cake",
     description: "Bông lan vani cùng kem sữa chua, mứt dâu và trái cây tươi",
     taste: "Ngọt",
     size: "16cm",
@@ -37,12 +53,13 @@ var list_products = [{
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 4,
+    },
+{
+    id: 5,
     name: "Strawberry Brownie Mousse Cake",
-    price: "450000",
+    price: "420000",
     image: "image/product/STRAWBERRY-MOUSSE.png",
-    type:"Cake",
+    type:"Whole Cake",
     description: "Bông lan sô cô la, kem mousse sô cô la và caramel",
     taste: "Ngọt",
     size: "16cm",
@@ -50,170 +67,509 @@ var list_products = [{
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 5,
+    },
+{
+    id: 6,
+    name: "Fruity Butter Cream Cake",
+    price: "420000",
+    image: "image/product/FRUITY.png",
+    type:"Whole Cake",
+    description: "Bông lan kem bơ custard cùng các loại trái cây tươi",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 7,
+    name: "Choco Cheese Mousse Cake",
+    price: "450000",
+    image: "image/product/CHOCO-CHEESE.png",
+    type:"Whole Cake",
+    description: "Bánh kem sô cô la, mousse sô cô la và kem phô mai",
+    taste: "Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 8,
+    name: "Mango Coco Mousse Cake (Sliced)",
+    price: "400000",
+    image: "image/product/Mango-Coconut-Mousse-Cake.png",
+    type:"Short Cake",
+    description: "Bánh kem mousse dừa, xoài & custard xoài chanh dây",
+    taste: "Chua, Ngọt",
+    size: "16cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 9,
+    name: "Miroir Café Mousse Cake",
+    price: "80000",
+    image: "image/product/cafe.png",
+    type:"Short Cake",
+    description: "Bông lan sô cô la, kem mousse cà phê và caramel",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 10,
+    name: "Miroir Au Chocolat Mousse Cake",
+    price: "880000",
+    image: "image/product/chcoo.png",
+    type:"Short Cake",
+    description: "Bông lan sô cô la, kem mousse vani và sô cô la",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 11,
+    name: "Miroir Lime Mousse Cake",
+    price: "80000",
+    image: "image/product/lime.png",
+    type:"Short Cake",
+    description: "Bông lan vani, kem mousse chanh và dâu tây",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 12,
+    name: "Fresh Strawberry Yogurt Cake (Short)",
+    price: "90000",
+    image: "image/product/yogurt-sliced.png",
+    type:"Short Cake",
+    description: "Bông lan vani cùng kem sữa chua, mứt dâu và trái cây tươi",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 13,
+    name: "Tiramisu Cheesecake (Sliced)",
+    price: "120000",
+    image: "image/product/tira-sliced.png",
+    type:"Short Cake",
+    description: "Bánh kem tiramisu cà phê và kem phô mai béo ngậy",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 14,
+    name: "Black Forest Cake (Sliced)",
+    price: "100000",
+    image: "image/product/black-forest-sliced.png",
+    type:"Short Cake",
+    description: "Bánh kem sô cô la cùng kem tươi và mứt cherry",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 15,
     name: "Brioche Bread",
-    price: "80000" ,
+    price: "120000",
     image: "image/product/brioche.png",
-    type:"Bread",
+    type:"Bread and Pastry",
     description: "Bánh mì hoa cúc thơm ngon phủ hạnh nhân",
     taste: "Ngọt",
-    texture: "Mềm, xốp",
-    size: "Phù hợp cho một bữa ăn",
+    size: "22cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 6,
+    },
+     {
+    id: 16,
     name: "Fresh Cream Loaf Bread",
-    price: "75000",
+    price: "320000",
     image: "image/product/fresh-cream-loaf.png",
-    type:"Bread",
+    type:"Bread and Pastry",
     description: "Bánh gối nhân kem sữa đặc ngọt dịu",
     taste: "Ngọt",
-    texture: "Mềm, xốp",
-    size: "Phù hợp cho một bữa ăn",
+    size: "20cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 7,
+    },
+      {
+    id: 17,
     name: "Best Ever Garlic Bread",
-    price: "60000",
+    price: "140000",
     image: "image/product/garlic-butter.png",
-    type:"Bread",
+    type:"Bread and Pastry",
     description: "Bánh mì bơ tỏi kem phô mai",
-    taste: "Ngọt",
-    texture: "Giòn, xốp",
-    size: "Phù hợp ăn vặt",
+    taste: "Mặn",
+    size: "12cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 8,
-    name: "Butter Croissant",
-    price: "69000",
-    image: "image/product/croissant.png",
-    type:"Bread",
+    },
+       {
+    id: 18,
+    name: "Mini Butter Croissant (5Pcs)",
+    price: "35000",
+    image: "image/product/mini-croissant.png",
+    type:"Bread and Pastry",
     description: "Bánh Croissant Bơ ngàn lớp thơm ngon",
     taste: "Ngọt",
-    texture: "Giòn, xốp",
-    size: "Phù hợp ăn vặt",
+    size: "10cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 9,
-    name: "Walnut Chocolate Cookie",
-    price: "19000",
-    image: "image/product/choco-walnut.png",
-    type:"Cookie",
-    description: "Cookie sô cô la giòn tan cùng hạt óc chó thơm ngon",
+    },
+        {
+    id: 19,
+    name: "Mini Choco Pastry Twists (8Pcs)",
+    price: "40000",
+    image: "image/product/chocochi.png",
+    type:"Bread and Pastry",
+    description: "Những chiếc bánh ngàn lớp xoắn sô cô la chip mini",
     taste: "Ngọt",
-    texture: "Giòn, xốp",
-    size: "Phù hợp ăn vặt",
+    size: "7cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 10,
-    name: "Cranberry Walnut Cookie",
-    price: "19000",
-    image: "image/product/cranberry-walnut.png",
-    type:"Cookie",
-    description: "Cookie giòn tan cùng hạt óc chó và nam việt quất",
+    },
+         {
+    id: 20,
+    name: "Korean Milk Cream Bread",
+    price: "20000",
+    image: "image/product/korean-milke.png",
+    type:"Bread and Pastry",
+    description: "Bánh mì tươi cùng nhân kem sữa ngọt dịu",
     taste: "Ngọt",
-    texture: "Giòn, xốp",
-    size: "Phù hợp ăn vặt",
+    size: "12cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 11,
-    name: "English Toffee Cookie",
-    price: "19000",
-    image: "image/product/toffee.png",
-    type:"Cookie",
-    description: "Cookie giòn tan cùng kẹo bơ béo ngậy",
+    },
+          {
+    id: 21,
+    name: "Corn Custard Donut",
+    price: "30000",
+    image: "image/product/corn-donut.png",
+    type:"Bread and Pastry",
+    description: "Bánh Donut kem custard Bắp và bắp tươi",
     taste: "Ngọt",
-    texture: "Giòn, xốp",
-    size: "phù hợp ăn vặt",
+    size: "16cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 12,
-    name: "Double Chocochip Cookie",
-    price: "19000",
-    image: "image/product/chocochip.png",
-    type:"Cookie",
+    },
+{
+    id: 22,
+    name: "Chocolate Macaron",
+    price: "10000",
+    image: "image/product/choco-macaron-1.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem tươi sô cô la",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 23,
+    name: "Strawberry Macaron",
+    price: "10000",
+    image: "image/product/bery-macaron-1.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem tươi mứt dâu tây",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 24,
+    name: "Yuzu Macaron",
+    price: "10000",
+    image: "image/product/yuzu-macaron-1.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem tươi thanh yên",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 25,
+    name: "Vanilla Macaron",
+    price: "110000",
+    image: "image/product/vani-macaron.png",
+    type:"Dessert",
+    description: "Bánh Macaron giòn tan cùng kem vani",
+    taste: "Ngọt",
+    size: "5cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 26,
+    name: "Mango Royal Pudding",
+    price: "20000",
+    image: "image/product/mango-pudding.png",
+    type:"Dessert",
+    description: "Lớp Pudding mềm mại, mịn màng cùng mứt xoài chua ngọt",
+    taste: "Ngọt",
+    size: "200ml",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 27,
+    name: "Original Royal Pudding",
+    price: "20000",
+    image: "image/product/original-pudding.png",
+    type:"Dessert",
+    description: "Lớp Pudding mịn màng vị vani cùng caramel",
+    taste: "Ngọt",
+    size: "200ml",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+{
+    id: 28,
+    name: "Mr.Bear Madeleine",
+    price: "15000",
+    image: "image/product/Mr.Bear_.png",
+    type:"Gifts",
+    description: "Bánh Madeleine mềm mại vị sô cô la hình chú Gấu",
+    taste: "Ngọt",
+    size: "15cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 29,
+    name: "Ms.Bear Madeleine 2",
+    price: "15000",
+    image: "image/product/Ms.Bear2_.png",
+    type:"Gifts",
+    description: "Bánh Madeleine mềm mại vị vani hình chú Gấu",
+    taste: "Ngọt",
+    size: "15cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 30,
+    name: "Premium Honey Castella",
+    price: "22000",
+    image: "image/product/cSSTEALL.png",
+    type: "Gifts",
+    description: "Bông lan Castella mềm mịn vị mật ong",
+    taste: "Ngọt",
+    size: "10cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 31,
+    name: "Silky Roll Cake",
+    price: "30000",
+    image: "image/product/SILKY.png",
+    type: "Gifts",
+    description: "Bông lan cuộn nhân kem vani và nho khô thượng hạng",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 32,
+    name: "Choco Banana Roll Cake",
+    price: "30000",
+    image: "image/product/CHOCO.png",
+    type: "Gifts",
+    description: "Bông lan sô cô la thượng hạng nhập khẩu Hàn Quốc",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 33,
+    name: "Velvet Strawberry Roll Cake",
+    price: "30000",
+    image: "image/product/VELVET.png",
+    type: "Gifts",
+    description: "Bánh bông lan dâu tây cuộn kem tươi dâu tây",
+    taste: "Ngọt",
+    size: "20cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 34,
+    name: "Caramel Walnut Pound Cake",
+    price: "50000",
+    image: "image/product/WALNUT-POUND.png",
+    type: "Gifts",
+    description: "Bánh Pound Cake Caramel và Hạt Óc Chó",
+    taste: "Ngọt",
+    size: "22cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+    {
+    id: 35,
+    name: "Double Chocochip Cookie (Box)",
+    price: "60000",
+    image: "image/product/double.png",
+    type:"Cookies",
     description: "Cookie giòn tan cùng sô cô la đậm vị",
     taste: "Ngọt",
-    texture: "Giòn, xốp",
-    size: "phù hợp ăn vặt",
+    size: "6cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 13,
-    name: "Chicken Mango Tropical Salad",
-    price: "79000",
-    image: "image/product/Mango-Chicken-Salad.png",
-    type:"Sandwich and Salad",
-    description: "Salad gà nướng, xoài, bắp, cà cùng sốt dầu giấm",
-    taste: "Chua, Ngọt",
-    size: "Phù hợp cho một bữa ăn",
+    },
+     {
+    id: 36,
+    name: "English Toffee Cookie (Box)",
+    price: "60000",
+    image: "image/product/cranberry-walnut-box.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng kẹo bơ béo ngậy",
+    taste: "Ngọt",
+    size: "6cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 14,
-    name: "B.E.L.T. Sandwich",
-    price: "45000",
-    image: "image/product/BELT.png",
-    type:"Sandwich and Salad",
-    description: "Sandwich thịt xông khói, trứng, xà lách và cà chua",
-    taste: " Mặn",
-    texture: "Mềm, xốp",
-    size: "Phù hợp cho một bữa ăn",
-    promo: {
-        name: "Giảm 10%",
-        discount: 10,
-    }
-}, {
-    id: 15,
-    name: "Cranbery Chicken Pastry Wrap Sandwich",
-    price: "69000",
-    image: "image/product/cranberry-chicken.png",
-    type:"Sandwich and Salad",
-    description: "Sandwich ngàn lớp cùng thịt gà và hạt Nam việt quất",
+    },
+      {
+    id: 337,
+    name: "Macadamia Cookie",
+    price: "7000",
+    image: "image/product/macadamia-1.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng hạt mắc ca bổ dưỡng",
     taste: "Mặn",
-    texture: "Mềm, xốp",
-    size: "Phù hợp cho một bữa ăn",
+    size: "6cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}, {
-    id: 16,
-    name: "Spicy Chicken & Avocado Salad",
-    price: "79000",
-    image: "image/product/AVOCADO-SALAD.png",
-    type:"Sandwich and Salad",
-    description: "Salad gà chiên giòn sốt cay cùng bơ tươi và sốt hành mayo",
-    taste: "Mặn",
-    size: "Phù hợp cho một bữa ăn",
+    },
+       {
+    id: 38,
+    name: "English Toffee Cookie",
+    price: "7000",
+    image: "image/product/toffee.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng kẹo bơ béo ngậy",
+    taste: "Ngọt",
+    size: "7cm",
     promo: {
         name: "Giảm 10%",
         discount: 10,
     }
-}]
+    },
+        {
+    id: 339,
+    name: "Double Chocochip Cookie",
+    price: "7000",
+    image: "image/product/chocolate.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng sô cô la đậm vị",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+         {
+    id: 40,
+    name: "Cranberry Walnut Cookie",
+    price: "7000",
+    image: "image/product/cranberry-walnut.png",
+    type:"Cookies",
+    description: "Cookie giòn tan cùng hạt óc chó và nam việt quất",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+          {
+    id: 41,
+    name: "Walnut Chocolate Cookie",
+    price: "7000",
+    image: "image/product/choco-walnut.png",
+    type:"Cookies",
+    description: "Cookie sô cô la giòn tan cùng hạt óc chó thơm ngon",
+    taste: "Ngọt",
+    size: "6cm",
+    promo: {
+        name: "Giảm 10%",
+        discount: 10,
+    }
+    },
+]
